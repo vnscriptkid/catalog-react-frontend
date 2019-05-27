5eabb77c01e343fa89439f139f0f6f98991ef06c
Thanh Nguyen <vnscriptkid@gmail.com>
2019-05-27 18:02:20 +0700

    change build/ to docs/

---
+++ docs/precache-manifest.f20ccfe617e1c89349f28918e81f8922.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53cd3cf1ad9e151c8ddb27b6d830522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceafce498606a83a99c4",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "f7cb669d2e08f4785cae",
+    "url": "/static/js/2.fe2139cf.chunk.js"
+  },
+  {
+    "revision": "ceafce498606a83a99c4",
+    "url": "/static/js/main.2ec4f13e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
